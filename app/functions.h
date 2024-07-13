084b3ecc58ca6a496d26ad72ceb0cf5c8bc6ec90
Lukas Hilfiker <lukas@hilfiker.dev>
2024-07-13 21:52:03 +0200

    functions.h: refrence public functions from log and index class.

---
+++ app/functions.h
@@ -0,0 +1,14 @@
+// functions.h
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+#include <filesystem>
+
+// log.cpp
+void log.write(int log_level, std::string log_message);
+
+// index.cpp
+void index.save_config(std::filesystem::path config_index_path, int config_buffer_size);
+int initialize();
+int uninitialize();
