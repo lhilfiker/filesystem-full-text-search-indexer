23397ca8e2ee4cf64da8b8f0d4e84331cd329efa
Lukas Hilfiker <lukas@hilfiker.dev>
2024-07-14 14:57:48 +0200

    main: move tests to seperate test function and call that

--- app/main.cpp
+++ app/main.cpp
@@ -3,6 +3,13 @@
 #include <filesystem>
 
 int main() {
+	if (test == 1) {
+		return 1;
+	}
+	return 0;
+}
+
+int test() {
 	log::write(2, "main: starting application. loading config...");
 	log::save_config(1);
 	index::save_config("~/.local/share/filesystem-full-text-search-indexer/", 500000);
