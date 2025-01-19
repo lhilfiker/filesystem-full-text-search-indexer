dcad68cb612b0c767497c06a1ae73ccdd64200c4
Lukas Hilfiker <lukas@hilfiker.dev>
2025-01-19 17:01:16 +0100

    Index: add new cleanup - better comments - don't resize at the end as we can calculate the exact space needed at the beginning if LocalIndex properly records them.

--- app/Index.cpp
+++ app/Index.cpp
@@ -329,12 +329,13 @@ int Index::add_new(index_combine_data &index_to_add) {
   log::write(2, "Index: add: first time write.");
   // paths
   uint64_t file_location = 0;
+  // resize files to make enough space to write all the data
   paths_size_buffer = (index_to_add.paths.size() * 2) + index_to_add.paths_size;
   paths_count_size_buffer = index_to_add.paths_count_size;
   words_size_buffer =
       index_to_add.words_size + index_to_add.words_reversed.size();
   // words_f_size maybe needs to be extended to allow larger numbers if 8
-  // bytes turn out to be too small. maybe automaticly resize if running out
+  // bytes turn out to be too small. maybe automatically resize if running out
   // of space?
   words_f_size_buffer = 26 * 8; // uint64_t stored as 8 bytes(64 bits) for
                                 // each letter in the alphabet.
@@ -349,6 +350,8 @@ int Index::add_new(index_combine_data &index_to_add) {
         (additional_reversed_counter.reversed.size() + 19) / 24;
   }
   additional_size_buffer *= 50;
+
+  // resize
   unmap();
   resize(index_path / "paths.index", paths_size_buffer);
   resize(index_path / "paths_count.index", paths_count_size_buffer);
@@ -357,6 +360,8 @@ int Index::add_new(index_combine_data &index_to_add) {
   resize(index_path / "reversed.index", reversed_size_buffer);
   resize(index_path / "additional.index", additional_size_buffer);
   map();
+
+  // write all paths to disk with a offset in beetween.
   for (const std::string &path : index_to_add.paths) {
     PathOffset offset = {};
     offset.offset = path.length();
@@ -369,11 +374,13 @@ int Index::add_new(index_combine_data &index_to_add) {
       ++file_location;
     }
   }
-  paths_size = file_location; // -1 to remove the last newline character
-  index_to_add.paths.clear(); // free memory
+  paths_size = file_location;
+  index_to_add.paths.clear(); // free memory as we have written this to memory already and don't need it anymore.
 
   log::write(2, "Index: add: paths written.");
+
   // paths_count
+  // write the word count as 4 bytes values each to disk.
   file_location = 0;
   for (const uint32_t &path_count : index_to_add.paths_count) {
     mmap_paths_count[file_location] = (path_count >> 24) & 0xFF;
@@ -386,17 +393,20 @@ int Index::add_new(index_combine_data &index_to_add) {
   index_to_add.paths_count.clear(); // free memory
 
   log::write(2, "Index: add: paths_count written.");
+
   // words & words_f & reversed
   std::array<WordsFValue, 26> words_f = {0};
   char current_char = '0';
   file_location = 0;
 
   for (const words_reversed &word : index_to_add.words_reversed) {
+    // check if the first char is different from the last. if so, save the location to words_f. words_reversed is sorted already.
     if (word.word[0] !=
         current_char) { // save file location if a new letter appears.
       current_char = word.word[0];
       words_f[current_char - 'a'].value = file_location;
     }
+    // We use a 1byte seperator beetween each word. We also directly use it to indicate how long the word is.
     size_t word_length = word.word.length();
     if (word_length + 30 >
         254) { // if the word is under 225 in length we can save it directly,
@@ -418,8 +428,9 @@ int Index::add_new(index_combine_data &index_to_add) {
   // write words_f
   std::vector<int> to_set;
   // check words_f. If any character is 0, set it to the value of the next non
-  // 0 word.
+  // 0 word. This can happen if no word with a specific letter occured. We set it to the next char start.
   for (int i = 1; i < 26; ++i) { // first one is always 0 so we skip it.
+    // we add it to a list of items with 0. If we find one with a number we write all in the list to the same number. At the end if there are still 0 in the list we write it with the last location of the file.
     if (words_f[i].value == 0) {
       to_set.push_back(i);
     } else {
@@ -442,20 +453,23 @@ int Index::add_new(index_combine_data &index_to_add) {
 
   words_f_size = file_location;
   log::write(2, "indexer: add: words_f written");
+
   // reversed & additional
   file_location = 0;
   size_t additional_file_location = 0;
   size_t additional_id = 1;
   for (const words_reversed &reversed : index_to_add.words_reversed) {
-    // it just needs a reversed block and no additional.
+    // check if we need only a reversed block or also additionals.
     ReversedBlock current_ReversedBlock{};
     if (reversed.reversed.size() <= 4) {
+      // we just need a reversed so we will write that.
       int i = 0;
       for (const uint32_t &r_id : reversed.reversed) {
         current_ReversedBlock.ids[i] = r_id + 1; // paths are indexed from 1
         ++i;
       }
     } else {
+      // create additional blocks and write if full. At the end write current additional that has not been written.
       int additional_i = 0;
       int reversed_i = 0;
       AdditionalBlock additional{};
@@ -500,14 +514,7 @@ int Index::add_new(index_combine_data &index_to_add) {
   reversed_size = file_location;
   log::write(2, "indexer: add: reversed and additonal written");
 
-  unmap();
-  resize(index_path / "paths_count.index", paths_count_size);
-  resize(index_path / "paths.index", paths_size);
-  resize(index_path / "words.index", words_size);
-  resize(index_path / "words_f.index", words_f_size);
-  // no resizing of reversed and additional as we can calculate its needed
-  // space at the beginning.
-  map();
+  // we do not need to resize because we already know the size accuratly.
   first_time = false;
 
   if (ec) {
