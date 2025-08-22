d5049be98fdc7ffcbe9a6c3af4d0c3c9b8150012
Lukas Hilfiker <lukas@hilfiker.dev>
2025-08-22 15:42:59 +0200

    config load

--- app/Config/Config.cpp
+++ app/Config/Config.cpp
@@ -1,4 +1,5 @@
 #include "config.h"
+#include "../Logging/logging.h"
 #include <filesystem>
 #include <map>
 #include <string>
@@ -23,11 +24,27 @@ void Config::update_value(std::pair<std::string, std::string> overwrite) {
   }
 }
 
-bool Config::validate() {}
+bool Config::validate() { return true; }
 
 void Config::set() {}
 
 void Config::load(std::vector<std::pair<std::string, std::string>> overwrites,
                   std::filesystem::path config_file_path) {
   // overwrites is a key<->value list.
+
+  read_config(config_file_path);
+
+  // overwrite with cli options
+  for (auto &element : overwrites) {
+    update_value(element);
+  }
+
+  if (!validate()) {
+    Log::error("Invalid config. Required options not set. Please set either in "
+               "your config file or using commandline options. If you need "
+               "help look at the documentation.");
+  }
+
+  // finally send the new values to the other classes.
+  set();
 }
