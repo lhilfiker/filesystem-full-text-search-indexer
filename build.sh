aebb84619d6a787bca613363db0b06d0625fa0dd
Lukas Hilfiker <lukas@hilfiker.dev>
2025-08-30 11:28:48 +0200

    pr feedback

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ echo "Building..."
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j$(nproc 2>/dev/null || echo 4)
+make -j$(nproc 2>/dev/null || echo 2)
 
 echo
 echo "Build complete!"
